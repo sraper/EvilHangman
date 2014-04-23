a69a42f3cce17e56e580adf3b4edf080f36c387a
Sam Raper <sraper@seas.upenn.edu>
2014-04-23 04:08:48 -0400

    part 1

--- src/GUI_Loser.java
+++ src/GUI_Loser.java
@@ -1,48 +1,26 @@
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
+import java.util.Set;
+
 import javax.sound.sampled.*;
 
-public class GUI_Loser implements ActionListener
-{
-    private JFrame parentFrame;
-    private JFrame loserFrame;
-    private JLabel secretWordLabel;
-    private JLabel gameResultLabel;
-    private JButton rtnBtn;
+public class GUI_Loser extends GUI_EndScreen {
 
-    public GUI_Loser(String Letters, JFrame frame)
-    {
-       
-        parentFrame = frame;
-        loserFrame = new JFrame("You are the loser!");
-        loserFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        loserFrame.setSize(new Dimension(300,470));
-        loserFrame.setLayout(new FlowLayout());
-        
-        secretWordLabel = new JLabel("The answer is "+Letters+".");
-        gameResultLabel = new JLabel("You are the Loser!");
-        rtnBtn = new JButton("Return to the main menu");
-        
-        rtnBtn.addActionListener(this); 
-        
-        ImageIcon icon = new ImageIcon("loser.gif"); 
-        JLabel loserPic = new JLabel(icon);
+	public GUI_Loser(String answer, JFrame frame) {
+		super(answer, frame);
+	}
 
-        
-        loserFrame.add(secretWordLabel);
-        loserFrame.add(gameResultLabel);
-        loserFrame.add(rtnBtn);
-        loserFrame.add(loserPic);
-        
-        loserFrame.setVisible(true);
-    }
-    
-    public void actionPerformed(ActionEvent e)
-    {
-        loserFrame.dispose(); //close the window
-        parentFrame.dispose(); // and the parent
-    	new Start().createAndShowGUI(); // start over
-    }
+	@Override
+	void setUpLabels(String answer) {
+		JLabel secretWordLabel = new JLabel("The answer is " + answer + ".");
+      	JLabel gameResultLabel = new JLabel("You are the Loser!");
+	    JLabel loserPic = new JLabel(new ImageIcon("loser.gif"));
+	    
+	    mainFrame.add(secretWordLabel);
+	    mainFrame.add(gameResultLabel);
+	    mainFrame.add(loserPic);
+	}	
 }
